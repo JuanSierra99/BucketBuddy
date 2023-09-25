d421c41f851c540e88bb25e125d398c028b774b2
Juan Sierra <sierra.juan99@gmail.com>
2023-09-24 22:13:50 -0700

    css tweaks + added page routing

--- bucketbuddy/src/App.jsx
+++ bucketbuddy/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import "./App.css";
 import { BucketBuddyLink } from "./components/BucketBuddyLink.jsx";
 import FloatingInput from "./components/FloatingInput";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
     <body>
       <nav className="top-bar">
         <BucketBuddyLink link="http://www.bing.com" />
-        <img src="/src/assets/react.svg" style={{}} />
+        {/* <img src="/src/assets/react.svg" style={{}} /> */}
         <a href="https://react.dev">Sign in</a>
       </nav>
       <img src="/buddy.png" className="buddy-image" />
