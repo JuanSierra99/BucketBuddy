5d662924ae158d8e1111d33f5feca9c6606a4198
Juan Sierra <sierra.juan99@gmail.com>
2023-10-31 01:22:09 -0700

    Making table a component
    We need to make table a component. We want the table to re-render with the appropriate data when we select a different table. May need to be split up into even smaller components later.

---
+++ src/components/atoms/TextInput.jsx
@@ -0,0 +1,3 @@
+export default function TextInput({ value, type }) {
+  return <input type={type} defaultValue={value}></input>;
+}
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -8,65 +8,66 @@ const url = "http://localhost:3000";
 
 const getJson = async (url) => {
   const response = await fetch(url);
-      if (response.ok) {
-        console.log(response.status + " " + response.statusText);
-        const json = await response.json();
-        return json
-      } else {
-        console.log("Error: " + response.status + " " + response.statusText);
-      }
-}
+  if (response.ok) {
+    console.log(response.status + " " + response.statusText);
+    const json = await response.json();
+    return json;
+  } else {
+    console.log("Error: " + response.status + " " + response.statusText);
+  }
+};
 
-export function HomePage() {
+export function Table({selectedTable}) {
   const [dataSet, setDataSet] = useState([{}]);
   const [tables, setTables] = useState([{}]);
 
   useEffect(() => {
     const getTable = async () => {
-      const table_name = "test";
+      const table_name = selectedTable;
       const url = `http://localhost:3000/table?name=${table_name}`;
-      const json = await getJson(url)
-      setDataSet(json)
-    }
+      const json = await getJson(url);
+      setDataSet(json);
+    };
     const getTables = async () => {
-      const url = "http://localhost:3000/api/tables"
-      const json = await  getJson(url)
-      setTables(json.table_names)
-    }
+      const url = "http://localhost:3000/api/tables";
+      const json = await getJson(url);
+      setTables(json.table_names);
+    };
     getTable();
     getTables();
   }, []);
+
   //initialized to an array with one empty object
   //therefore if dataSet is never changed, keys.map will not render any <th> elements
   //similarly for dataset.map, that uses same dataSet array, so it also wont render anything
-  const keys = Object.keys(dataSet[0]); 
-
+  const keys = Object.keys(dataSet[0]);
   return (
-    <div>
-      {JSON.stringify(tables)}
-      <table>
-        {/*A table header for every key*/}
-        {keys.map((k) => {
-          return <th>{k}</th>;
-        })}
+    <table>
+      {/*A table header for every key*/}
+      {keys.map((k) => {
+        return <th>{k}</th>;
+      })}
 
-        {/*for every object in our data set*/}
-        {dataSet.map((data, n) => {
-          return (
-            <tr key={n}>
-              {/*display the value for every key in the object*/}
-              {keys.map((k, n) => {
-                return (
-                  <td>
-                    <input key={n} type="text" value={data[k]} />
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </table>
-      {/* <p>{JSON.stringify(dataSet)}</p> */}
-    </div>
+      {/*for every object in our data set*/}
+      {dataSet.map((data, n) => {
+        return (
+          <tr key={n}>
+            {/*display the value for every key in the object*/}
+            {keys.map((k, n) => {
+              return (
+                <td>
+                  <input key={n} type="text" value={data[k]} />
+                </td>
+              );
+            })}
+          </tr>
+        );
+      })}
+    </table>
   );
+}
+
+export function HomePage() {
+  const [selectedTable, setSelectedTable] = useState("test")
+  return (<Table selectedTable={selectedTable}/>);
 }
