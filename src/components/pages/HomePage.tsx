9d70ba562845a720721225557179e6b36c4b3046
Juan Sierra <sierra.juan99@gmail.com>
2023-11-03 18:29:20 -0700

    Update HomePage.tsx
    changed the way we select tables. we use a scrolling sidebar made with buttons now. Divided the page for the sidebar to exist.

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -32,19 +32,25 @@ const Post = async (url, json) => {
   }
 };
 
-// Component that gives dropdown of all tables 
+// Component that gives dropdown of all tables
 const Selector = (props) => {
-  const data = props.data
+  const data = props.data;
   return (
-    <select
-      onChange={(e) => {
-        props.setState(e.target.value);
-      }}
-    >
+    <div className="scrollable-div">
       {data.map((name) => {
-        return <option value={name}>{name}</option>;
+        return (
+          <button
+            className="tableNameButton"
+            value={name}
+            onClick={(e) => {
+              props.setState(e.target.value);
+            }}
+          >
+            {name}
+          </button>
+        );
       })}
-    </select>
+    </div>
   );
 };
 
@@ -57,44 +63,51 @@ export function HomePage() {
     const url = "http://localhost:3000/api/all-tables";
     const json = await getJson(url);
     setTables(json.table_names);
-    return json.table_names
+    return json.table_names;
   };
   //retrieve a list of all the tables in our database
   //maybe we can use this to prevent users from trying to create a duplicate table with api create table endpoint
   useEffect(() => {
-     getTables().then((res) => {setSelectedTable(res[0])});
+    getTables().then((res) => {
+      setSelectedTable(res[0]);
+    });
   }, []); //maybe we want to do something with this when we add new tables ?
 
   return (
     <div>
-      <Selector setState={setSelectedTable} data={tables}/>
-      <input
-        type="text"
-        id="createTableName"
-        placeholder="Enter new table name"
-        value={newTableName}
-        onChange={(e) => setNewTableName(e.target.value)}
-      />
-      <button
-        onClick={async () => {
-          const url = "http://localhost:3000/api/new-table"
-          const json = { name: newTableName }
-          await Post(url, json); //requests api endpoint to create new table. must await for getTables() to have updated info
-          await getTables(); //will request api endpoint to send current tables in db, then updates tables state
-        }}
-      >
-        Create new table
-      </button>
-      <button
-        onClick={async () => {
-          const url = "http://localhost:3000/api/add-row"
-          const json = { tableName: "movies" }
-          await Post(url, json); //requests api endpoint to create new table. must await for getTables() to have updated info
-        }}
-      >
-        Add Row
-      </button>
-      <Table selectedTable={selectedTable} />
+      <div className="sidebar">
+        <Selector setState={setSelectedTable} data={tables} />
+      </div>
+      <div className="rightbar">
+        <input
+          type="text"
+          id="createTableName"
+          placeholder="Enter new table name"
+          value={newTableName}
+          onChange={(e) => setNewTableName(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            const url = "http://localhost:3000/api/new-table";
+            const json = { name: newTableName };
+            await Post(url, json); //requests api endpoint to create new table. must await for getTables() to have updated info
+            await getTables(); //will request api endpoint to send current tables in db, then updates tables state
+          }}
+        >
+          Create new table
+        </button>
+        <button
+          onClick={async () => {
+            const url = "http://localhost:3000/api/add-row";
+            const json = { tableName: selectedTable };
+            await Post(url, json); //requests api endpoint to create new table. must await for getTables() to have updated info
+          }}
+        >
+          Add Row
+        </button>
+        <p className="databaseStatus">Database status</p>
+        <Table selectedTable={selectedTable} />
+      </div>
     </div>
   );
 }
