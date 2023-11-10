a216aa3e2ed18d730bd528076dd152a3f681ba6a
Juan Sierra <sierra.juan99@gmail.com>
2023-11-09 17:51:21 -0800

    Added simple login and register endpoints
    Also added a bunch of necessary returns. better format

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,13 @@
 const client = require("./database");
+const express = require("express");
+const cors = require("cors");
+const passport = require("./passport-config");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // Api server framework
-const express = require("express");
 const app = express();
-const cors = require("cors");
+
 // const { table } = require("console");
 
 // Needed for cross origin requests
@@ -14,7 +18,7 @@ app.use(
 );
 
 app.use(express.json()); //Needed to parse JSON request bodies
-// app.use(express.urlencoded({ extended: true })); // Needed when we use post in html form
+app.use(express.urlencoded({ extended: true })); // Needed when we use post in html form
 
 const port = 3000; // Run this api server on port 3000
 app.listen(port, () => {
@@ -22,17 +26,15 @@ app.listen(port, () => {
 });
 
 // Send row data of the specific table
-app.get("/api/get-table", async (req, res) => {
+app.get("/api/get-table", async (req, response) => {
   try {
     const table_name = req.query.name;
     if (!table_name) {
       console.log("Table name is required");
-      res.status(400).json({ Error: "Table name is required" });
-      return;
+      return response.status(400).json({ Error: "Table name is required" });
     }
     if (!isValidName(table_name)) {
-      res.status(400).json({ Error: "Invalid table name " });
-      return;
+      return response.status(400).json({ Error: "Invalid table name " });
     }
     const query = `SELECT * FROM "${table_name}"`;
     // Execute the SQL query to get the specified table's rows, and assing it to data
@@ -41,10 +43,10 @@ app.get("/api/get-table", async (req, res) => {
     // Log the data for debugging
     console.log(`Sending rows for ${table_name}: `, rows);
     // Send an HTTP response with data in the response body
-    res.status(200).json(rows);
+    return response.status(200).json(rows);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ error: "Internal Server Error" });
+    return response.status(500).json({ error: "Internal Server Error" });
   }
 });
 
@@ -54,13 +56,11 @@ app.get("/api/table-fields", async (request, response) => {
     const table_name = request.query.name;
     if (!table_name) {
       console.log("table name required");
-      response.status(400).json({ message: "table name required" });
-      return;
+      return response.status(400).json({ message: "table name required" });
     }
     if (!isValidName(table_name)) {
       console.log("Invalid table name");
-      response.status(400).json({ message: "Invalid table name" });
-      return;
+      return response.status(400).json({ message: "Invalid table name" });
     }
     const query = `SELECT column_name FROM information_schema.columns WHERE table_name='${table_name}' AND column_name!='unique_record_id'`;
     const json = await client.query(query);
@@ -68,10 +68,10 @@ app.get("/api/table-fields", async (request, response) => {
       return row.column_name;
     });
     console.log("Sending fields for " + table_name + " table: ", fields);
-    response.status(200).json(fields);
+    return response.status(200).json(fields);
   } catch (error) {
     console.log(error.message);
-    response.status(500).json({ error: error.message });
+    return response.status(500).json({ error: error.message });
   }
 });
 
@@ -83,14 +83,12 @@ app.post("/api/new-table", async (request, response) => {
     // Ensure that the given table name is defined
     if (!tableName) {
       console.log("Table name is required");
-      response.status(400).json({ error: "Table name is required" });
-      return;
+      return response.status(400).json({ error: "Table name is required" });
     }
     // Ensure that the given table name is valid
     if (!isValidName(tableName)) {
       console.log("Invalid table name");
-      response.status(400).json({ error: "Invalid table name" });
-      return;
+      return response.status(400).json({ error: "Invalid table name" });
     }
     const createTableQuery = `CREATE TABLE "${tableName}" (unique_record_id SERIAL PRIMARY KEY , Name VARCHAR)`;
     // Execute the SQL query to create the table
@@ -103,7 +101,7 @@ app.post("/api/new-table", async (request, response) => {
       .json({ message: `Table "${tableName}" created successfully` });
   } catch (error) {
     console.log(error.message);
-    response.status(500).json({ error: "Table creation failed" });
+    return response.status(500).json({ error: "Table creation failed" });
   }
 });
 
@@ -122,23 +120,19 @@ app.post("/api/change-cell", async (request, response) => {
     // Ensure that the given table name is defined
     if (!tableName) {
       console.log("Table name is required");
-      response.status(400).json({ error: "Table name is required" });
-      return;
+      return response.status(400).json({ error: "Table name is required" });
     }
     if (!rowId) {
       console.log("Row Id is required");
-      response.status(400).json({ error: "Row Id is required" });
-      return;
+      return response.status(400).json({ error: "Row Id is required" });
     }
     if (!fieldName) {
       console.log("Field name is required");
-      response.status(400).json({ error: "Field name is required" });
-      return;
+      return response.status(400).json({ error: "Field name is required" });
     }
     if (!newCellValue) {
       console.log("New cell value is required");
-      response.status(400).json({ error: "New cell value is required" });
-      return;
+      return response.status(400).json({ error: "New cell value is required" });
     }
     // Validate all recieved parameters
     if (
@@ -148,8 +142,7 @@ app.post("/api/change-cell", async (request, response) => {
       !isValidName(newCellValue)
     ) {
       console.log("Invalid Parameter");
-      response.status(400).json({ error: "Invalid Parameter" });
-      return;
+      return response.status(400).json({ error: "Invalid Parameter" });
     }
     const changeCellQuery = `UPDATE "${tableName}" SET "${fieldName}" = '${newCellValue}' WHERE unique_record_id=${rowId}`;
     // Execute the SQL query to create the table
@@ -159,12 +152,12 @@ app.post("/api/change-cell", async (request, response) => {
       `Changed cell in table "${tableName}", with row id ${rowId}, and field name "${fieldName}", to ${newCellValue}`
     );
     // Send an HTTP response with a success message in the response body
-    response.status(200).json({
+    return response.status(200).json({
       message: `Changed cell in table "${tableName}", with row id ${rowId}, and field name "${fieldName}", to ${newCellValue}`,
     });
   } catch (error) {
     console.log("Error: " + error.message);
-    response.status(500).json({ error: error.message });
+    return response.status(500).json({ error: error.message });
   }
 });
 
@@ -179,18 +172,17 @@ app.post("/api/change-field", async (request, response) => {
     // Ensure that the given table name is defined
     if (!tableName) {
       console.log("Table name is required");
-      response.status(400).json({ error: "Table name is required" });
-      return;
+      return response.status(400).json({ error: "Table name is required" });
     }
     if (!fieldName) {
       console.log("Current field name is required");
-      response.status(400).json({ error: "Current field name is required" });
-      return;
+      return response
+        .status(400)
+        .json({ error: "Current field name is required" });
     }
     if (!newFieldName) {
       console.log("New field name is required");
-      response.status(400).json({ error: "New field name is required" });
-      return;
+      return response.status(400).json({ error: "New field name is required" });
     }
     // Validate all recieved parameters
     if (
@@ -199,8 +191,7 @@ app.post("/api/change-field", async (request, response) => {
       !isValidName(newFieldName)
     ) {
       console.log("Invalid Parameter");
-      response.status(400).json({ error: "Invalid Parameter" });
-      return;
+      return response.status(400).json({ error: "Invalid Parameter" });
     }
     const changeFieldNameQuery = `ALTER TABLE "${tableName}" RENAME COLUMN "${fieldName}" TO "${newFieldName}"`;
     // Execute the SQL query to create the table
@@ -213,7 +204,7 @@ app.post("/api/change-field", async (request, response) => {
     });
   } catch (error) {
     console.log("Error: " + error.message);
-    response.status(500).json({ error: error.message });
+    return response.status(500).json({ error: error.message });
   }
 });
 
@@ -231,10 +222,10 @@ app.get("/api/all-tables", async (req, response) => {
     // Log the data being sent for debugging
     console.log("Sending table names: ", table_names);
     // Send an HTTP response with an object containing the array of table names in the response body
-    response.status(200).json({ table_names: table_names });
+    return response.status(200).json({ table_names: table_names });
   } catch (error) {
     console.log(error.message);
-    response.status(500).json({ error: "Internal Server Error" });
+    return response.status(500).json({ error: "Internal Server Error" });
   }
 });
 
@@ -246,18 +237,15 @@ app.post("/api/add-column", async (request, response) => {
     // Ensure all values are defined.
     if (!tableName) {
       console.log("Table name required");
-      response.status(400).json({ Error: "Table name required" });
-      return;
+      return response.status(400).json({ Error: "Table name required" });
     }
     if (!columnName) {
       console.log("Column name required");
-      response.status(400).json({ Error: `Column name required` });
-      return;
+      return response.status(400).json({ Error: `Column name required` });
     }
     if (!dataType) {
       console.log("data type required");
-      response.status(400).json({ Error: "data type required" });
-      return;
+      return response.status(400).json({ Error: "data type required" });
     }
     // Validate each value before adding it to the query
     if (
@@ -266,8 +254,7 @@ app.post("/api/add-column", async (request, response) => {
       !isValidName(dataType)
     ) {
       console.log("Invalid query");
-      response.status(400).json({ Error: "invalid query" });
-      return;
+      return response.status(400).json({ Error: "invalid query" });
     }
     // Execute the SQL query to add a column to the specified table
     await client.query(
@@ -276,7 +263,7 @@ app.post("/api/add-column", async (request, response) => {
     // Log a success message
     console.log(`Added '${columnName}' ${dataType} Field, to '${tableName}'`);
     // Send an HTTP response with a success message in the response body
-    response.status(200).json({
+    return response.status(200).json({
       message: `Added '${columnName}' ${dataType} Field, to table '${tableName}'`,
     });
   } catch (error) {
@@ -285,13 +272,13 @@ app.post("/api/add-column", async (request, response) => {
       console.log(
         `Column "${columnName}" already exists in table "${tableName}"`
       );
-      response.status(400).json({
+      return response.status(400).json({
         error: `Column "${columnName}" already exists in table "${tableName}"`,
       });
     } else {
       console.log(error.message);
       // Send a HTTP response containing a generic error message for other errors.
-      response.status(500).json({ error: "Internal Server Error" });
+      return response.status(500).json({ error: "Internal Server Error" });
     }
   }
 });
@@ -301,29 +288,94 @@ app.post("/api/add-row", async (request, response) => {
     const tableName = request.body.tableName;
     if (!tableName) {
       console.log("Table name is required");
-      response.status(400).json({ Error: "Table name is required" });
-      return;
+      return response.status(400).json({ Error: "Table name is required" });
     }
     if (!isValidName(tableName)) {
       console.log("Invalid table name");
-      response.status(400).json({ Error: "invalid table name" });
+      return response.status(400).json({ Error: "invalid table name" });
     }
     newRowQuery = `INSERT INTO "${tableName}" DEFAULT VALUES`;
     await client.query(newRowQuery);
     console.log(`Added new row to table: ${tableName}`);
-    response
+    return response
       .status(200)
       .json({ message: `Added new row to table: ${tableName}` });
   } catch (error) {
     console.log({ error: error.message });
-    response
+    return response
       .status(500)
       .json({ error: "Internal Server Error", message: error.message });
   }
 });
 
+app.post("/register", async (request, response) => {
+  try {
+    const { username, password } = request.body;
+    if (!username) {
+      console.log("message: username required");
+      return response.status(400).json({ Error: "username required" });
+    }
+    if (!isValidUsername(username)) {
+      console.log("invalid username");
+      return response.status(400).json({ Error: "invalid username" });
+    }
+    if (!password) {
+      console.log("password required");
+      return response.json({ Error: "password required" });
+    }
+    await client.query(
+      `INSERT INTO users (username, password) VALUES ('${username}', '${password}') `
+    );
+    console.log("Success: new user registered");
+    return response.status(200).json({ Success: "New user registered" });
+  } catch (error) {
+    console.log("Error: ", error.message);
+    return response.json({ Error: error.message });
+  }
+});
+
+app.post("/login", async (request, response) => {
+  try {
+    const username = request.body.username;
+    if (!username) {
+      console.log("message: username required");
+      return response.status(400).json({ Error: "username required" });
+    }
+    if (!isValidUsername(username)) {
+      console.log("invalid username");
+      return response.status(400).json({ Error: "invalid username" });
+    }
+    const result = await client.query(
+      `SELECT username FROM users where username='${username}'`
+    );
+    if (result.rows.length === 0) {
+      console.log("User not found");
+      return response.status(404).json({ error: "User not found" });
+    }
+    const user = {
+      username: username,
+    };
+    const secret_key = process.env.JWT_SIGN_KEY; // used here to encode the jwt
+    const token = jwt.sign(user, secret_key, { expiresIn: 10 });
+    console.log("sending jwt");
+    return response.json({ token }); // issue the jwt in response
+  } catch (error) {
+    console.log("Error: ", error.message);
+    return response.json({ Error: error.message });
+  }
+});
+
 // Validate strings to ensure query safety. (IS THIS GOOD ENOUGH ??)
 function isValidName(tableName) {
+  // one way to define regular expressions is with literals like this
   const validNameRegex = /^[a-zA-Z0-9_ ]+$/;
+  // test the string
   return validNameRegex.test(tableName);
+}
+
+function isValidUsername(username) {
+  // One way to define regular expression is with constructor like this
+  const validUserRegex = new RegExp("^[a-zA-Z0-9_.]{3,15}$");
+  // test the string
+  return validUserRegex.test(username);
 }
