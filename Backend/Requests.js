d7e81e5df272a73b0041d28a1ff2a78ae7ee8ded
Juan Sierra <sierra.juan99@gmail.com>
2023-11-09 17:52:45 -0800

    Addeed authorization header to get and post requests

--- Backend/Requests.js
+++ Backend/Requests.js
@@ -1,8 +1,15 @@
 export const getJson = async (url) => {
-  const response = await fetch(url);
+  const token = localStorage.getItem("jwtToken");
+  const response = await fetch(url, {
+    method: "Get",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
   if (response.ok) {
     // we return the json
-    console.log(response.status + " " + response.statusText);
+    // console.log(response.status + " " + response.statusText);
     const json = await response.json();
     return json;
   } else {
@@ -13,10 +20,12 @@ export const getJson = async (url) => {
 };
 
 export const Post = async (url, json) => {
+  const token = localStorage.getItem("jwtToken");
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(json),
   });
