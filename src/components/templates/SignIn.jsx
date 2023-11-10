1efaa6cd8ddbf2a6d0f0a73df45f7ea96262a301
Juan Sierra <sierra.juan99@gmail.com>
2023-11-10 15:01:54 -0800

    set up Authentication using login endpoint in api

--- src/components/templates/SignIn.jsx
+++ src/components/templates/SignIn.jsx
@@ -1,10 +1,21 @@
 import FloatingInput from "../atoms/FloatingInput";
 import { NavBar } from "../molecules/NavBar";
 import { SignUpLink } from "../atoms/SignUpLink";
+import { Post } from "../../../Backend/Requests";
+import { useState } from "react";
 
 function SignIn() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const url = "http://localhost:3000/login";
+    const user = document.getElementById("username").value;
+    const sendJson = { username: user };
+    const response = await Post(url, sendJson); // post request, wait for json response w/ token
+    localStorage.removeItem("jwtToken");
+    localStorage.setItem("jwtToken", response.token); // save the jwt to local storage. Now it can be used to authenticate all our requests
+  };
   return (
-    <body
+    <div
       style={{
         backgroundImage:
           'URL("https://static.vecteezy.com/system/resources/previews/010/527/184/original/liquid-flow-purple-purple-3d-neon-lava-lamp-geometric-background-for-banner-card-ui-design-or-wallpaper-gradient-mesh-bubble-in-the-shape-of-a-wave-drop-fluid-colorful-3d-tubes-free-vector.jpg")',
@@ -15,16 +26,26 @@ function SignIn() {
       <NavBar navItem={<SignUpLink />}></NavBar>
       <img src="/buddy-smile.png" className="buddy-image" />
       <div className="form-container">
-        <form>
-          <FloatingInput value="username" type="email" id="username" />
-          <FloatingInput value="password" type="password" id="password" />
+        <form onSubmit={handleSubmit}>
+          <FloatingInput
+            value="username"
+            type="text"
+            id="username"
+            name="username"
+          />
+          <FloatingInput
+            value="password"
+            type="password"
+            id="password"
+            name="password"
+          />
           <input className="submit-form-button" type="submit" value="sign in" />
         </form>
-        <a href="http://www.bing.com" class="forgot-password-link">
+        <a href="http://www.bing.com" className="forgot-password-link">
           Forgot Password?
         </a>
       </div>
-    </body>
+    </div>
   );
 }
 
