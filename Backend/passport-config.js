d6c792f7293afaa46190bec6ee7042b8b6215bb2
Juan Sierra <sierra.juan99@gmail.com>
2023-11-10 17:37:07 -0800

    Removed db query. Dont need it
    If you have already verified the user’s existence in the database during the login process, and you trust the information in the issued JWT, there is typically no need to revalidate the user’s existence in the database when they access your endpoints using the JWT.

--- Backend/passport-config.js
+++ Backend/passport-config.js
@@ -6,31 +6,18 @@ require("dotenv").config();
 
 //Configure JWT options
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SIGN_KEY,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // how we will extract jwt
+  secretOrKey: process.env.JWT_SIGN_KEY, // used to verify the signature of the incoming JWT
 };
 
 // https://github.com/jwalton/passport-api-docs
 passport.use(
   new JwtStrategy(opts, (jwtPayload, done) => {
     const username = jwtPayload.username;
-    client.query(
-      `SELECT username FROM users WHERE username='${username}'`, // Only obtain username
-      (err, result) => {
-        if (err) {
-          return done(err, false);
-        }
-        if (result.rows.length === 1) {
-          const user = {
-            username: jwtPayload.username,
-          };
-          // const user = result.rows[0];
-          return done(null, user); // user is added to request object in our api endpoints
-        } else {
-          return done(null, false);
-        }
-      }
-    );
+    const user = {
+      username: jwtPayload.username,
+    };
+    return done(null, user);
   })
 );
 
