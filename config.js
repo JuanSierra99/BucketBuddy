cdb9211218b70c95c92d24e9b640c2499bbaf20d
Juan Sierra <sierra.juan99@gmail.com>
2023-11-11 00:29:22 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  serverUrl: "http://13.58.117.253:3000",
-};
+const serverUrl = "http://13.58.117.253:3000";
+// const serverUrl = "http://localhost:3000";
+export default serverUrl;
