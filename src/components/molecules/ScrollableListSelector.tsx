8ba908276291c03beeac58fc058e8a312c2ea238
Juan Sierra <sierra.juan99@gmail.com>
2023-11-17 11:31:21 -0800

    Restructured jsx a little for styling. Also can now get table colors from api !

--- src/components/molecules/ScrollableListSelector.tsx
+++ src/components/molecules/ScrollableListSelector.tsx
@@ -1,45 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./ScrollableListSelector.css";
 // This component displays a scrollable list of table names.
 // It sets the state of the selected table based on user interaction with the list.
 // Additionally, it includes search functionality to filter the displayed tables.
 export const ScrollableListSelector = (props) => {
-    const data = props.data;
-    const [lastSelected, setLastSelected] = useState(null);
-    const [searchTableName, setSearchTableName] = useState("");
-    return (
-      <div className="scrollable-div">
-        <input
-          className="search-bar"
-          type="text"
-          id="searchForTable"
-          placeholder="Search"
-          value={searchTableName}
-          onChange={(e) => setSearchTableName(e.target.value)}
-        />
-        {data.map((name) => {
-          // Only return tables that contains value in search bar
-          if (name.toLowerCase().includes(searchTableName.toLocaleLowerCase())) {
-            return (
-              <div>
-                <button
-                  className="tableNameButton"
-                  value={name}
-                  onClick={(e) => {
-                    props.setState(e.target.value);
-                    setLastSelected(name);
-                  }}
-                >
-                  {name}
-                  {lastSelected === name && ( // if last selected table is name of currently selected table. SHOW THE DOG !!!
-                    <img src="/button-buddy.PNG" className="button-buddy-image" />
-                  )}
-                </button>
-              </div>
-            );
-          }
-        })}
-      </div>
-    );
-  };
-  
+  const data = props.data;
+  const [lastSelected, setLastSelected] = useState(null);
+  const [searchTableName, setSearchTableName] = useState("");
+  return (
+    <div className="scrollable-div">
+      <input
+        className="search-bar"
+        type="text"
+        id="searchForTable"
+        placeholder="Search"
+        value={searchTableName}
+        onChange={(e) => setSearchTableName(e.target.value)}
+      />
+      {data.map((name) => {
+        // Only return tables that contains value in search bar
+        if (
+          name.table_name
+            .toLowerCase()
+            .includes(searchTableName.toLocaleLowerCase())
+        ) {
+          return (
+            <button
+              style={{ color: name.table_color }}
+              className="tableNameButton"
+              value={name.table_name}
+              onClick={(e) => {
+                props.setState(e.target.value);
+                setLastSelected(name.table_name);
+              }}
+            >
+              {name.table_name}
+              {lastSelected === name.table_name && ( // if last selected table is name of currently selected table. SHOW THE DOG !!!
+                <img src="/button-buddy.PNG" className="button-buddy-image" />
+              )}
+            </button>
+          );
+        }
+      })}
+    </div>
+  );
+};
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,62 +3,80 @@ import Table from "../molecules/Table";
 import { ScrollableListSelector } from "../molecules/ScrollableListSelector";
 import { getJson, Post } from "../../../Backend/Requests";
 import { serverUrl } from "../../config";
+import "./HomePage.css";
 import { NavBar } from "../molecules/NavBar";
 
 export function HomePage() {
   const [selectedTable, setSelectedTable] = useState("");
   const [tables, setTables] = useState([]);
   const [newTableName, setNewTableName] = useState("");
+  const [tableColor, setTableColor] = useState("orange");
 
   const getTables = async () => {
     const apiUrl = `${serverUrl}/api/all-tables`;
     const json = await getJson(apiUrl);
-    setTables(json.table_names); // set state with the obtained table names
-    return json.table_names;
+    setTables(json.table_data); // set state with the obtained table names
+    return json.table_data;
   };
   //retrieve a list of all the tables in our database
   //maybe we can use this to prevent users from trying to create a duplicate table with api create table endpoint
   useEffect(() => {
     getTables().then((res) => {
       if (res.length > 0) {
-        setSelectedTable(res[0]);
+        setSelectedTable(res[0].table_name);
       }
     });
   }, []); //maybe we want to do something with this when we add new tables ?
 
   return (
     <div className="homepage">
+      <NavBar></NavBar>
       <div className="sidebar">
-      <input
-          type="text"
-          id="createTableName"
-          placeholder="Enter new table name"
-          value={newTableName}
-          onChange={(e) => setNewTableName(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            const apiUrl = `${serverUrl}/api/new-table`;
-            const json = { table_name: newTableName };
-            await Post(apiUrl, json); //requests api endpoint to create new table. must await for getTables() to have updated info
-            await getTables(); //will request api endpoint to send current tables in db, then updates tables state
-          }}
-        >
-          Create new table
-        </button>
+        <div className="create-table-container">
+          <input
+            className="create-table-input"
+            type="text"
+            id="createTableName"
+            placeholder="new table name"
+            value={newTableName}
+            onChange={(e) => setNewTableName(e.target.value)}
+          />
+          <button
+            className="create-table-button"
+            onClick={async () => {
+              const apiUrl = `${serverUrl}/api/new-table`;
+              const json = {
+                table_name: newTableName,
+                table_color: tableColor,
+              };
+              await Post(apiUrl, json); //requests api endpoint to create new table. must await for getTables() to have updated info
+              await getTables(); //will request api endpoint to send current tables in db, then updates tables state
+            }}
+          >
+            Create new table
+          </button>
+        </div>
         <ScrollableListSelector setState={setSelectedTable} data={tables} />
       </div>
-      <div className="rightbar">
-        <button onClick={async () => {
-          const apiUrl = `${serverUrl}/api/deleteTable`
-          const json = { table_name : selectedTable}
-          await Post(apiUrl, json)
-          getTables();
-        }}>
+      <div className="table-section">
+        <input
+          type="color"
+          onChange={(e) => {
+            setTableColor(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={async () => {
+            const apiUrl = `${serverUrl}/api/deleteTable`;
+            const json = { table_name: selectedTable, table_color: tableColor };
+            await Post(apiUrl, json);
+            getTables();
+          }}
+        >
           Delete table
         </button>
         <Table selectedTable={selectedTable} />
-         <p className="databaseStatus">Database status</p>
+        <p className="databaseStatus">Database status</p>
       </div>
     </div>
   );
