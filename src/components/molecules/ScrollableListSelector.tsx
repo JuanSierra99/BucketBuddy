33fae9fcba7f2db762f3f8045a80f786e01fdd47
Juan Sierra <sierra.juan99@gmail.com>
2023-11-17 11:35:43 -0800

    rename variable

--- src/components/molecules/ScrollableListSelector.tsx
+++ src/components/molecules/ScrollableListSelector.tsx
@@ -4,7 +4,7 @@ import "./ScrollableListSelector.css";
 // It sets the state of the selected table based on user interaction with the list.
 // Additionally, it includes search functionality to filter the displayed tables.
 export const ScrollableListSelector = (props) => {
-  const data = props.data;
+  const tables_array = props.data;
   const [lastSelected, setLastSelected] = useState(null);
   const [searchTableName, setSearchTableName] = useState("");
   return (
@@ -17,25 +17,25 @@ export const ScrollableListSelector = (props) => {
         value={searchTableName}
         onChange={(e) => setSearchTableName(e.target.value)}
       />
-      {data.map((name) => {
+      {tables_array.map((table) => {
         // Only return tables that contains value in search bar
         if (
-          name.table_name
+          table.table_name
             .toLowerCase()
             .includes(searchTableName.toLocaleLowerCase())
         ) {
           return (
             <button
-              style={{ color: name.table_color }}
+              style={{ color: table.table_color }}
               className="tableNameButton"
-              value={name.table_name}
+              value={table.table_name}
               onClick={(e) => {
                 props.setState(e.target.value);
-                setLastSelected(name.table_name);
+                setLastSelected(table.table_name);
               }}
             >
-              {name.table_name}
-              {lastSelected === name.table_name && ( // if last selected table is name of currently selected table. SHOW THE DOG !!!
+              {table.table_name}
+              {lastSelected === table.table_name && ( // if last selected table is name of currently selected table. SHOW THE DOG !!!
                 <img src="/button-buddy.PNG" className="button-buddy-image" />
               )}
             </button>
