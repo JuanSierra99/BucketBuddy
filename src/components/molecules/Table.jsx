d9449640223b04c4f937cd7755b1d7f9dc344f0d
Juan Sierra <sierra.juan99@gmail.com>
2023-11-27 19:51:29 -0800

    Error catching. Started row filtering with checkbox input.
    Ensure no duplicate field names exist. A new field name state has been introduced. If the new field name is valid and the field is created, the input box is cleared. The process of adding functionality to filter the table has begun. Additionally, data type selection names have been modified for better clarity in understanding the chosen data type.

--- src/components/molecules/Table.jsx
+++ src/components/molecules/Table.jsx
@@ -11,6 +11,8 @@ export default function Table({ selectedTable }) {
   const [rows, setRows] = useState([{}]); // The row data for the selected table
   const [fields, setFields] = useState([]); // An object representing table fields. Has a name, and data type
   const [dataType, setDataType] = useState("VARCHAR"); // Used for creating new columns. changes when user wants to select a different data type.
+  const [filters, setFilters] = useState([]); // Filter functions applied from each field, that filter rows shown
+  const [newFieldName, setNewFieldName] = useState("");
 
   // Make api request to get rows for the selected table.
   const getRows = async (table_name) => {
@@ -27,6 +29,7 @@ export default function Table({ selectedTable }) {
       setFields(new_fields); //update the fields state
     }
   };
+
   // Make a Post request to add a new row to the table. Refreshes table to show new row
   const addRow = async () => {
     const apiUrl = `${serverUrl}/api/add-row`;
@@ -39,24 +42,53 @@ export default function Table({ selectedTable }) {
 
   // Make a Post request to add a new field/column to the table. Refreshes table to show new column
   const addField = async () => {
+    if (fields.includes(newFieldName)) {
+      console.log("Column Name already exists !"); // Make sure the column name is unique
+      return;
+    }
     const apiUrl = `${serverUrl}/api/add-column`;
     const json = {
       tableName: selectedTable.table_name,
-      columnName: dataType,
+      columnName: newFieldName,
       dataType,
     };
-    await Post(apiUrl, json); //requests api endpoint to alter table.
-    getFields(selectedTable.table_name);
+    const request = await Post(apiUrl, json); //requests api endpoint to alter table.
+    if (request) {
+      //
+      setNewFieldName("");
+      getFields(selectedTable.table_name);
+    } else {
+      console.log("Failed to add new column ");
+    }
+  };
+
+  //Apply a filter to the rows, so it's easier for users to see data they want
+  const addCheckboxFilter = async (filter, field) => {
+    if (filter === "true") {
+      setFilters([...filters, (row) => row[field] === true]);
+    }
+    if (filter === "false") {
+      setFilters([...filters, (row) => row[field] === false]);
+    }
   };
 
   // Get the data (rows and fields) for the table, and continue to do so whenever a new table is selected
   useEffect(() => {
     const table_name = selectedTable.table_name;
     if (table_name) {
-      getRows(table_name);
-      getFields(table_name);
+      getRows(table_name); // get tables rows
+      getFields(table_name); // get tables fields
+      setFilters([]); // unapply all filters
     }
   }, [selectedTable.table_name]);
+
+  useEffect(() => {
+    // apply each filter to rows state
+    const filteredRows = filters.reduce((accumulatedRows, currentFilter) => {
+      return accumulatedRows.filter(currentFilter); // same as rows.filter((row) => some filter condition)
+    }, rows);
+    setRows(filteredRows);
+  }, [filters]);
 
   const changeField = (tableName, currentFieldName, newFieldName) => {
     const apiUrl = `${serverUrl}/api/change-field`;
@@ -73,39 +105,68 @@ export default function Table({ selectedTable }) {
         New Entry
       </button>
       <button className="top-table-button" onClick={addField}>
-        Add Column
+        Add Column:
       </button>
+      <input // allow user to input new field name
+        id="new-field-name-input"
+        value={newFieldName}
+        placeholder="Enter Column Name"
+        onChange={(e) => {
+          setNewFieldName(e.target.value);
+        }}
+      ></input>
       <select
-        className="top-table-button"
         id="data-type"
         onChange={(e) => {
           setDataType(e.target.value);
         }}
       >
-        <option value={"VARCHAR"}>VarChar</option>
+        <option value={"VARCHAR"}>Text Line</option>
+        <option value={"TEXT"}>Text Block</option>
         <option value={"INT"}>Number</option>
-        <option value={"BOOL"}>Boolean</option>
+        <option value={"MONEY"}>Money</option>
+        <option value={"BOOL"}>CheckBox</option>
         <option value={"DATE"}>Date</option>
-        <option value={"JSON"}>JSON</option>
-        <option value={"TIME"}>TIME</option>
-        <option value={"MONEY"}>MONEY</option>
-        <option value={"TEXT"}>TEXTBLOCK</option>
+        {/* <option value={"JSON"}>JSON</option> */}
+        <option value={"TIME"}>Time</option>
       </select>
+      <button
+        className="top-table-button"
+        onClick={() => getRows(selectedTable.table_name)}
+      >
+        undo filters
+      </button>
       <table style={{ backgroundColor: selectedTable.table_color }}>
         {/*make every key in our table a header*/}
         {fields.map((field_data, index) => {
           return (
             <th>
-              <input
+              {/* <input
                 type="text"
                 value={field_data.column_name}
-                // onChange={(e) => {
-                //   const newFields = [...fields];
-                //   newFields[index] = e.target.value;
-                //   setFields(newFields);
-                // }}
-                // onBlur={(e) => {changeField(selectedTable, e.)}}
-              ></input>
+                onChange={(e) => {
+                  const newFields = [...fields];
+                  newFields[index] = e.target.value;
+                  setFields(newFields);
+                }}
+                onBlur={(e) => {}}
+              ></input> */}
+              <p onBlur={(e) => {}}>{field_data.column_name}</p>
+              {field_data.data_type === "boolean" && (
+                <select
+                  id="select-filter"
+                  onChange={async (e) =>
+                    await addCheckboxFilter(
+                      e.target.value,
+                      field_data.column_name
+                    )
+                  }
+                >
+                  <option value="none">-</option>
+                  <option value="true">checked</option>
+                  <option value="false">unchecked</option>
+                </select>
+              )}
             </th>
           );
         })}
